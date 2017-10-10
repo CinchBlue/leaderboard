cc3c4e9266028eb9043e2663e9862708c9aad5bf
Austin Tasato <aytasato@gmail.com>
2017-10-10 10:11:07 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+PlayersList = new Mongo.Collection('players');
+
+if (Meteor.isClient) {
+  Template.leaderboard.events({
+    'click .player': function() {
+      let playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+
+  Template.leaderboard.helpers({
+    'player': function() {
+      return PlayersList.find();
+    },
+    'selectedClass': function() {
+      let playerId = this._id;
+      let selectedPlayer = Session.get('selectedPlayer');
+      if (playerId == selectedPlayer) {
+        return 'selected';
+      }
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  console.log('Hello server');
+}
